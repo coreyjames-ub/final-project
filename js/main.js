c6e52924740b497957dd1c57911b5211b7871070
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-04-26 10:23:14 -0600

    i know how to get a url in JS

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // import accessToken, { accessToken } from './module.js';
 import * as ApiKey from './apiKey.js';
 let accessToken = ApiKey.getAccessToken();
-// import {clientId} from './module.mjs';
+let clientId = ApiKey.getClientId();
 // import {clientSecret} from './module.mjs';
 
 // API FUNCTIONS
@@ -13,8 +13,25 @@ async function stravaLoggedInUser() {
     console.log(data);
 }
 
-stravaLoggedInUser();
+// stravaLoggedInUser();
 
-async function stravaReadAll(){
-    let url = 'https://www.strava.com/oauth/authorize?client_id=83079&client_secret=1e2f9f71f5c0af93c7e542f3beb377f58e618dcf'
+async function stravaCode(){
+    let url = `http://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read_all`;
 }
+
+async function stravaOAuth(){
+    
+    let httpRes = await fetch(url);
+    let data = await httpRes.json();
+    console.log(data);
+}
+
+// stravaOAuth();
+
+function  logUrl() {
+    let url = window.location.href;
+    console.log(url);
+}
+
+logUrl();
+
